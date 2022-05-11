708bc414b4910c6229f7b3bc703b74375faf4274
MateBook <examletru@gmail.com>
2022-05-11 11:06:16 +0300

    first commit

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -62,7 +62,7 @@ const toggleMobileMenu = () => (mobileMenuCollapsed.value = !mobileMenuCollapsed
               <div mb-4px class="ml-10px">отзывы и результаты</div>
             </button>
           </div>
-          <div class="h-32px mt-14px" w-full text-center flex flex-row items-center text-14px justify-between>
+          <div class="h-32px mt-14px" w-full text-center flex flex-row items-center text-14px justify-between px-10px>
             <div fw-500>8 (800) 222-86-63</div>
             <div flex flex-row items-center gap-5px>
               <QLink active>whatsapp</QLink>
